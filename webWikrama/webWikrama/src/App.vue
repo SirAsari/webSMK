4fcb0b9c835692997e4cfc8eb0902ebf01dddb23
asari <muhamadasari@smkwikrama.sch.id>
2023-12-16 22:34:10 +0700

    Pushin P

---
+++ webWikrama/webWikrama/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <Header />
+  </div>
+</template>
+
+<script>
+// import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header'
+
+export default {
+    name: 'Header',
+    components: {
+      Header
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
